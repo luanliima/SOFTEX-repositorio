831367efe1630884d55d9b15f83557b3f0dfefea
Luan lima <87495302+luanliima@users.noreply.github.com>
2022-08-10 15:36:53 -0300

    classes e objetos

---
+++ classe.py
@@ -0,0 +1,16 @@
+class loja:
+    def __init__(self, cliente, funcionarios):
+        self.funionarios = funcionarios
+        self.cliente = cliente
+
+        def qtdClientes(self):
+            quantidade = self.Clientes
+            print(quantidade)
+
+        def qtdFuncionario(self):
+            quantidadefun = self.Funcionarios
+            print(quantidadefun)
+
+        objeto = loja(1000, 15)
+        print(object.quantidadeFun())
+        print(object.quantidade())
